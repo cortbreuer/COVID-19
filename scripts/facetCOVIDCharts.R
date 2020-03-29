6b79957549ba93606869996c1c6ae0e0b7deac84
Cort Breuer <cb732@cornell.edu>
2020-03-29 13:45:46 -0400

    update plots for March 29, reformat for data structure changes

--- scripts/facetCOVIDCharts.R
+++ scripts/facetCOVIDCharts.R
@@ -5,20 +5,20 @@ library(scales)
 library(here)
 library(ggsci)
 
-source(here("scripts", "getCases.R"))
+source("scripts/getCases.R")
 COVID <- as_tibble(COVID)
 
 #Limit dataset to 6 countries, mutate to get total case data
-limitedCOVID <- filter(COVID, GeoId %in% c('IT', 'ES', 'KR', 'US', 'FR', 'DE'))
+limitedCOVID <- filter(COVID, geoId %in% c('IT', 'ES', 'KR', 'US', 'FR', 'DE'))
 
 limitedCOVID <- mutate(limitedCOVID, Count = "daily")
 
-ID <- unique(limitedCOVID$GeoId)
+ID <- unique(limitedCOVID$geoId)
 limitedCOVIDSum <- data.frame()
 
 for(i in 1:length(ID)){
-  tempCountry <- limitedCOVID %>% filter(GeoId == ID[i]) %>% arrange(DateRep)
-  tempCountry[, "Cases"] <- cumsum(tempCountry$Cases)
+  tempCountry <- limitedCOVID %>% filter(geoId == ID[i]) %>% arrange(dateRep)
+  tempCountry[, "cases"] <- cumsum(tempCountry$cases)
   tempCountry[, "Count"] <- "total"
   limitedCOVIDSum <- bind_rows(limitedCOVIDSum, tempCountry)
 }
@@ -27,22 +27,22 @@ limitedCOVID <- bind_rows(limitedCOVID, limitedCOVIDSum)
 background_limitedCOVID <- select(limitedCOVID, -"Countries")
 
 #Plot faceted daily and total case values for 6 countries
-limitedCOVIDDaily <- ggplot(data = filter(limitedCOVID, Count == "daily"), mapping = aes(x = DateRep, y = Cases, color = Countries, group = GeoId)) + 
+limitedCOVIDDaily <- ggplot(data = filter(limitedCOVID, Count == "daily"), mapping = aes(x = dateRep, y = cases, color = Countries, group = geoId)) + 
   geom_line(data = filter(background_limitedCOVID, Count == "daily"), color = "grey", alpha = .8) + 
   geom_line(show.legend = FALSE) +
   facet_wrap(. ~ Countries) + 
   theme_bw() + 
   xlab("")
 
-limitedCOVIDTotal <- ggplot(data = filter(limitedCOVID, Count == "total"), mapping = aes(x = DateRep, y = Cases, color = Countries, group = GeoId)) + 
+limitedCOVIDTotal <- ggplot(data = filter(limitedCOVID, Count == "total"), mapping = aes(x = dateRep, y = cases, color = Countries, group = geoId)) + 
   geom_line(data = filter(background_limitedCOVID, Count == "total"), color = "grey", alpha = .8) + 
   geom_line(show.legend = FALSE) +
   facet_wrap(. ~ Countries) + 
   theme_bw() + 
   xlab("")
 
-ggsave(plot = limitedCOVIDDaily, here("figures", "2020-03-26_Limited-Facet_Daily-Cases.png"), width = 10, height = 6)
-ggsave(plot = limitedCOVIDTotal, here("figures", "2020-03-26_Limited-Facet_Case-Total.png"), width = 10, height = 6)
+ggsave(plot = limitedCOVIDDaily, "figures/2020-03-29_Limited-Facet_Daily-Cases.png", width = 10, height = 6)
+ggsave(plot = limitedCOVIDTotal, "figures/2020-03-29_Limited-Facet_Case-Total.png", width = 10, height = 6)
 
 #Mutate dataset to get total case data
 lateCOVID <- COVID
@@ -51,27 +51,27 @@ lateCOVID <- COVID
 
 lateCOVID <- mutate(lateCOVID, Count = "daily")
 
-ID <- unique(lateCOVID$GeoId)
+ID <- unique(lateCOVID$geoId)
 lateCOVIDSum <- data.frame()
 
 for(i in 1:length(ID)){
-  tempCountry <- lateCOVID %>% filter(GeoId == ID[i]) %>% arrange(DateRep)
-  tempCountry[, "Cases"] <- cumsum(tempCountry$Cases)
+  tempCountry <- lateCOVID %>% filter(geoId == ID[i]) %>% arrange(dateRep)
+  tempCountry[, "cases"] <- cumsum(tempCountry$cases)
   tempCountry[, "Count"] <- "total"
   lateCOVIDSum <- bind_rows(lateCOVIDSum, tempCountry)
 }
 
 lateCOVID <- bind_rows(lateCOVID, lateCOVIDSum)
-upperCOVID <- filter(lateCOVID, Cases >= 1400)
-upperID <- unique(upperCOVID$GeoId)
-lateCOVID <- filter(lateCOVID, GeoId %in% upperID)
+upperCOVID <- filter(lateCOVID, cases >= 1400)
+upperID <- unique(upperCOVID$geoId)
+lateCOVID <- filter(lateCOVID, geoId %in% upperID)
 background_lateCOVID <- select(lateCOVID, -"Countries")
 
 #Plot faceted daily and total case values
 
 ##Daily values
 ggplot(data = filter(lateCOVID, Count == "daily"), 
-       mapping = aes(x = DateRep, y = Cases, color = Countries, group = GeoId)) + 
+       mapping = aes(x = dateRep, y = cases, color = Countries, group = geoId)) + 
   geom_line(data = filter(background_lateCOVID, Count == "daily"), color = "grey", alpha = .8) + 
   geom_line(show.legend = FALSE) +
   facet_wrap(. ~ Countries) + 
@@ -79,7 +79,7 @@ ggplot(data = filter(lateCOVID, Count == "daily"),
   xlab("") 
 
 ##Total values
-ggplot(data = filter(lateCOVID, Count == "total"), mapping = aes(x = DateRep, y = Cases, color = Countries, group = GeoId)) + 
+ggplot(data = filter(lateCOVID, Count == "total"), mapping = aes(x = dateRep, y = cases, color = Countries, group = geoId)) + 
   geom_line(data = filter(background_lateCOVID, Count == "total"), color = "grey", alpha = .8) + 
   geom_line(show.legend = FALSE) +
   facet_wrap(. ~ Countries) + 
@@ -90,37 +90,37 @@ ggplot(data = filter(lateCOVID, Count == "total"), mapping = aes(x = DateRep, y 
 normalizedLateCOVID <- data.frame()
 
 for(i in 1:length(upperID)){
-  tempLateCOVID <- filter(lateCOVID, GeoId == upperID[i]) %>% arrange(DateRep)
-  tempLateCOVID[, "timeSince"] <- difftime(tempLateCOVID$DateRep, tempLateCOVID$DateRep[1], units = "days")
+  tempLateCOVID <- filter(lateCOVID, geoId == upperID[i]) %>% arrange(dateRep)
+  tempLateCOVID[, "timeSince"] <- difftime(tempLateCOVID$dateRep, tempLateCOVID$dateRep[1], units = "days")
   normalizedLateCOVID <- rbind(normalizedLateCOVID, tempLateCOVID)
 }
 
 background_normalizedLateCOVID <- select(normalizedLateCOVID, -"Countries")
 
 ##Daily values
-facetNormalizedCOVIDDaily <- ggplot(data = filter(normalizedLateCOVID, Count == "daily"), mapping = aes(x = timeSince, y = Cases, color = Countries, group = GeoId)) + 
+facetNormalizedCOVIDDaily <- ggplot(data = filter(normalizedLateCOVID, Count == "daily"), mapping = aes(x = timeSince, y = cases, color = Countries, group = geoId)) + 
   geom_line(data = filter(background_normalizedLateCOVID, Count == "daily"), color = "grey", alpha = .8) + 
   geom_line(show.legend = FALSE) +
   facet_wrap(. ~ Countries) + 
   theme_bw() + 
   xlab("Days Since First Case")
 
-ggsave(plot = facetNormalizedCOVIDDaily, here("figures", "2020-03-26_Facet_Daily-Cases.png"), width = 10, height = 7)
+ggsave(plot = facetNormalizedCOVIDDaily, "figures/2020-03-29_Facet_Daily-Cases.png", width = 10, height = 7)
 
 #Select for common start case number
-lateCOVID100 <- filter(normalizedLateCOVID, Cases >= 100)
+lateCOVID100 <- filter(normalizedLateCOVID, cases >= 100)
 normalizedLateCOVID100 <- data.frame()
 
 for(i in 1:length(upperID)){
-  tempLateCOVID100 <- filter(lateCOVID100, GeoId == upperID[i]) %>% arrange(DateRep)
-  tempLateCOVID100[, "timeSince"] <- difftime(tempLateCOVID100$DateRep, tempLateCOVID100$DateRep[1], units = "days")
+  tempLateCOVID100 <- filter(lateCOVID100, geoId == upperID[i]) %>% arrange(dateRep)
+  tempLateCOVID100[, "timeSince"] <- difftime(tempLateCOVID100$dateRep, tempLateCOVID100$dateRep[1], units = "days")
   normalizedLateCOVID100 <- rbind(normalizedLateCOVID100, tempLateCOVID100)
 }
 
 background_normalizedLateCOVID100 <- select(normalizedLateCOVID100, -"Countries")
 
 ##Total values
-facetNormalizedCOVIDTotal_lin <- ggplot(data = filter(normalizedLateCOVID100, Count == "total"), mapping = aes(x = timeSince, y = Cases, color = Countries, group = GeoId)) + 
+facetNormalizedCOVIDTotal_lin <- ggplot(data = filter(normalizedLateCOVID100, Count == "total"), mapping = aes(x = timeSince, y = cases, color = Countries, group = geoId)) + 
   geom_line(data = filter(background_normalizedLateCOVID100, Count == "total"), color = "grey", alpha = .8) + 
   geom_line(show.legend = FALSE) +
   facet_wrap(. ~ Countries) + 
@@ -128,7 +128,7 @@ facetNormalizedCOVIDTotal_lin <- ggplot(data = filter(normalizedLateCOVID100, Co
   xlim(0, 30) +
   xlab("")
 
-facetNormalizedCOVIDTotal_exp <- ggplot(data = filter(normalizedLateCOVID100, Count == "total"), mapping = aes(x = timeSince, y = Cases, color = Countries, group = GeoId)) + 
+facetNormalizedCOVIDTotal_exp <- ggplot(data = filter(normalizedLateCOVID100, Count == "total"), mapping = aes(x = timeSince, y = cases, color = Countries, group = geoId)) + 
   geom_line(data = filter(background_normalizedLateCOVID100, Count == "total"), color = "grey", alpha = .8) + 
   geom_line(show.legend = FALSE) +
   scale_y_log10(breaks = c(100, 1000, 10000, 100000), limits = c(100, 100000)) + 
@@ -138,8 +138,8 @@ facetNormalizedCOVIDTotal_exp <- ggplot(data = filter(normalizedLateCOVID100, Co
   theme_bw() + 
   xlab("Days Since 100th Case")
 
-ggsave(plot = facetNormalizedCOVIDTotal_lin, here("figures", "2020-03-26_Facet-Lin_Case-Total.png"), width = 10, height = 7)
-ggsave(plot = facetNormalizedCOVIDTotal_exp, here("figures", "2020-03-26_Facet-Exp_Case-Total.png"), width = 10, height = 7)
+ggsave(plot = facetNormalizedCOVIDTotal_lin, "figures/2020-03-29_Facet-Lin_Case-Total.png", width = 10, height = 7)
+ggsave(plot = facetNormalizedCOVIDTotal_exp, "figures/2020-03-29_Facet-Exp_Case-Total.png", width = 10, height = 7)
 
 
 
---
+++ scripts/loglogAnimation.R
@@ -0,0 +1,72 @@
+#Plot log scale daily cases and total cases for log-curve prediction
+library(tidyverse)
+library(gghighlight)
+library(scales)
+library(here)
+library(ggsci)
+library(TTR)
+library(ggsci)
+library(gganimate)
+library(gifski)
+
+source("scripts/getCases.R")
+COVID <- as_tibble(COVID)
+
+#Process data into sum form
+COVID <- rename(COVID, "dailyCases" = "cases", "dailyDeaths" = "deaths")
+
+ID <- unique(COVID$geoId)
+COVIDSum <- data.frame()
+
+for(i in 1:length(ID)){
+  tempCountry <- COVID %>% filter(geoId == ID[i]) %>% arrange(dateRep)
+  tempCountry[, "totalCases"] <- cumsum(tempCountry$dailyCases)
+  tempCountry[, "totalDeaths"] <- cumsum(tempCountry$dailyDeaths)
+  COVIDSum <- bind_rows(COVIDSum, tempCountry)
+}
+
+COVID <- COVIDSum
+
+COVIDTop <- filter(COVID, totalCases >= 2500)
+IDTop <- unique(COVIDTop$geoId)
+COVID <- filter(COVID, geoId %in% IDTop)
+COVIDAvg <- data.frame()
+
+for (i in 1:length(ID)){
+  tempCountry <- COVID %>% filter(geoId == IDTop[i]) %>% arrange(dateRep)
+  tempCountry[, "dailyCases"] <- frollmean(tempCountry$dailyCases, 5)
+  tempCountry[, "totalCases"] <- frollmean(tempCountry$totalCases, 5)
+  tempCountry[, "dailyDeaths"] <- frollmean(tempCountry$dailyDeaths, 5)
+  tempCountry[, "totalDeaths"] <- frollmean(tempCountry$totalDeaths, 5)
+  COVIDAvg <- bind_rows(COVIDAvg, tempCountry)
+}
+
+COVID <- COVIDAvg
+COVID <- filter(COVID, totalCases >= 10, totalDeaths >= 10)
+
+#Plot log-log values in animated form for cases and deaths
+
+loglogCasePlot <- ggplot(data = COVID) + 
+  geom_point(mapping = aes(x = totalCases, y = dailyCases, color = geoId)) +
+  geom_line(mapping = aes(x = totalCases, y = dailyCases, color = geoId)) + 
+  scale_y_log10(limits = c(10, 10000)) + 
+  scale_x_log10(limits = c(10, 100000)) + 
+  annotation_logticks(sides="lb") + 
+  transition_reveal(dateRep)
+
+animate(loglogCasePlot, renderer = gifski_renderer(loop = F), end_pause = 60, width = 800, height = 600)
+anim_save("../figures/loglogCasePlot.gif")
+
+loglogDeathPlot <- ggplot(data = COVID) + 
+  geom_point(mapping = aes(x = totalDeaths, y = dailyDeaths, color = geoId)) +
+  geom_line(mapping = aes(x = totalDeaths, y = dailyDeaths, color = geoId)) + 
+  scale_y_log10(limits = c(10, 1000)) + 
+  scale_x_log10(limits = c(10, 10000)) + 
+  annotation_logticks(sides="lb") + 
+  transition_reveal(dateRep)
+
+animate(loglogDeathPlot, renderer = gifski_renderer(loop = F), end_pause = 60, width = 800, height = 600)
+anim_save("../figures/loglogDeathPlot.gif", width = 8, height = 8)
+
+
+
